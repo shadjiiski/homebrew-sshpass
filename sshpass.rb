ccac303ad465cc4b31b9278ed093f5f8050db25a
Stanislav Hadjiiski <hadjiiskis@vmware.com>
2022-06-28 19:08:36 +0300

    Bump sshpass version to 1.09

--- sshpass.rb
+++ sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
-  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
+  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
 
   def install
     system "./configure", "--disable-debug",
